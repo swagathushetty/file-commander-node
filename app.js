b1d5a831253cc1491c826b2084ba5b29f67ee1f9
swagathushetty <swagath.shetty@gmail.com>
2024-12-08 19:50:06 +0530

    code cleanup

--- app.js
+++ app.js
@@ -4,22 +4,17 @@ const { off } = require('process');
 (async ()=>{
 
     const commandFileHandler = await fs.open("./command.txt","r")
-    const watcher = fs.watch("./command.txt")
 
-    for await(const event of watcher){
 
-            console.log(event)
-
-        if(event.eventType == "change"){
-
-             // heavy since it creates a large buffer and we dont ultise it fully
+    commandFileHandler.on("change",async ()=>{
+          // heavy since it creates a large buffer and we dont ultise it fully
             // const content = await commandFileHandler.read()
 
             //better memory efficent way
             const size = (await commandFileHandler.stat()).size // get file size
-            const buff = Buffer.alloc(size)
+            const buff = Buffer.alloc(size) //create buffer with size of file
             const offset = 0 // The location in the buffer at which to start filling.
-            const length = buff.byteLength
+            const length = buff.byteLength //how many bytes we want to read
             const position = 0 //The location where to begin reading data from the file
 
             const content = await commandFileHandler.read(
@@ -29,7 +24,15 @@ const { off } = require('process');
                 position
             )
             console.log(content)
+    })
+    const watcher = fs.watch("./command.txt")
 
+    for await(const event of watcher){
+
+            console.log(event)
+
+        if(event.eventType == "change"){
+            commandFileHandler.emit("change")
         }
         
     }
